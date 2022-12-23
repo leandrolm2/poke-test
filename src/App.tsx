d21d03374a70305cf6aea7c7d50a97be12a578c0
Leandro Melgarejo <leandromelgarejo@hotmail.com>
2022-12-23 12:02:35 -0300

    firts commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Props from './types/pokemons'
 
-function App() {
+
+const pokedex: React.FC<Props> = ({ pokemon }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{pokemon.name}</h1>
+      <p>{pokemon.type}</p>
+      <img src={pokemon.imageUrl} alt={pokemon.name} />
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default pokedex;
---
+++ src/types/pokemons.ts
@@ -0,0 +1,7 @@
+export default interface Props {
+    pokemon: {
+      name: string;
+      type: string;
+      imageUrl: string;
+    };
+}
